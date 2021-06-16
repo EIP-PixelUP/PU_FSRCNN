6292b6020583eafbececca7788feb31996f1c475
Jonathan Hugonnard <jonathan.hugonnard@gmail.com>
2021-06-16 18:49:50 +0200

    can now easily add datasets to download

--- download_dataset.py
+++ download_dataset.py
@@ -1,15 +1,26 @@
 #!/usr/bin/env python3
 
 import sys
+import json
 import requests
 
+def download_dataset(name, format, url):
+	request = requests.get(url)
+	if request.status_code != 200:
+		print("Request for", name, "failed, status code: ", request.status_code, file=sys.stderr)
+		return
+	try:
+		open('datasets/Dataset_'+name+"."+format, 'wb').write(request.content)
+	except Exception as e:
+		print("Saving file for", name, "failed. Error: ", str(e))
 
-request = requests.get("https://eip.epitech.eu/2023/pixelup/data/General-100.zip")
-if request.status_code != 200:
-	print("Request failed, status code: ", request.status_code, file=sys.stderr)
-	sys.exit(84)
 
-try:
-	open('Dataset_General100.zip', 'wb').write(request.content)
-except Exception as e:
-	print("Saving file failed. Error: ", str(e))
+if __name__ == "__main__":
+	try:
+		f = open("Datasets.json", 'r')
+		datasets = json.load(f)
+		for dataset in datasets['datasets']:
+			print("Downloading file: ", dataset['name'] + '.' + dataset['format'])
+			download_dataset(dataset['name'],dataset['format'],dataset['url'])
+	except Exception as e:
+		print("Opening and reading Datasets.json failed with this error: ", str(e))
