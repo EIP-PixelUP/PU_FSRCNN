a4fe0b551c5dc9aea582cc57db1dab1903aba443
Jonathan Hugonnard <jonathan.hugonnard@gmail.com>
2021-06-17 12:31:01 +0200

    comparing Images now takes PIL images as parameters

--- check.py
+++ check.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-from ast import parse
 from math import log10, sqrt
 import numpy as np
-import sys, argparse
+import argparse
 from PIL import Image
 
 def PSNR(original, compressed):
@@ -14,9 +13,9 @@ def PSNR(original, compressed):
 	psnr = 20 * log10(max_pixel / sqrt(mse))
 	return psnr
 
-def compareImages(firstImage : str, secondImage : str):
-	first = np.array(Image.open(firstImage))
-	second = np.array(Image.open(secondImage))
+def compareImages(firstImage : Image, secondImage : Image):
+	first = np.array(firstImage)
+	second = np.array(secondImage)
 	return PSNR(first, second)
 
 if __name__ == "__main__":
@@ -25,11 +24,10 @@ if __name__ == "__main__":
 	parser.add_argument('--upscaled', dest='upscaled', type=str, help="Upscaled image", required=True)
 	args = parser.parse_args()
 	try:
-		val = compareImages(args.original, args.upscaled)
+		val = compareImages(Image.open(args.original), Image.open(args.upscaled))
 		print("PSNR level of correspondance: ", val, "%")
 	except ValueError as e:
 		print("The two images must be of the same dimensions. Error: ", str(e))
 	except Exception as e:
 		print("Following error occured: ", str(e))
-
 
